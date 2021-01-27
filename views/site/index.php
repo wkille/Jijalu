924301bcef5b743015d4e53038a37ad776a55962
Wayne <contact@jimmajammalulu.com>
2021-01-27 17:38:45 +0000

    begin customising top of home page

--- views/site/index.php
+++ views/site/index.php
@@ -11,19 +11,7 @@ $this->title = 'Site main title';
     <div class="container">
         <div class="row">
             <div class="col-xl-9 mx-auto">
-                <h1 class="mb-5">Build a landing page for your business or project and generate more leads!</h1>
-            </div>
-            <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-                <form>
-                    <div class="form-row">
-                        <div class="col-12 col-md-9 mb-2 mb-md-0">
-                            <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
-                        </div>
-                        <div class="col-12 col-md-3">
-                            <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-                        </div>
-                    </div>
-                </form>
+                <h1 class="mb-5"><em>The town's</em> number one place for <em>things</em></h1>
             </div>
         </div>
     </div>
@@ -36,28 +24,28 @@ $this->title = 'Site main title';
             <div class="col-lg-4">
                 <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                     <div class="features-icons-icon d-flex">
-                        <i class="icon-screen-desktop m-auto text-primary"></i>
+                        <i class="icon-support m-auto text-primary"></i>
                     </div>
-                    <h3>Fully Responsive</h3>
-                    <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+                    <h3>Lifesavers</h3>
+                    <p class="lead mb-0">Bringing things back from the dead!</p>
                 </div>
             </div>
             <div class="col-lg-4">
                 <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                     <div class="features-icons-icon d-flex">
-                        <i class="icon-layers m-auto text-primary"></i>
+                        <i class="icon-wrench m-auto text-primary"></i>
                     </div>
-                    <h3>Bootstrap 4 Ready</h3>
-                    <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
+                    <h3>Fixing stuff</h3>
+                    <p class="lead mb-0">We fix broken things with wrenches!</p>
                 </div>
             </div>
             <div class="col-lg-4">
                 <div class="features-icons-item mx-auto mb-0 mb-lg-3">
                     <div class="features-icons-icon d-flex">
-                        <i class="icon-check m-auto text-primary"></i>
+                        <i class="icon-bubble m-auto text-primary"></i>
                     </div>
-                    <h3>Easy to Use</h3>
-                    <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                    <h3>24/7/365 support</h3>
+                    <p class="lead mb-0">We just love to chat!</p>
                 </div>
             </div>
         </div>
